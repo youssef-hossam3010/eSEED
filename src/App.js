64ba363b4507fe3a3e1b20eaf578f68cb7bdb918
youssef-hossam3010 <74060163+youssef-hossam3010@users.noreply.github.com>
2023-05-25 18:40:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { useState, useEffect } from 'react';
 import LineChart from './components/LineChart';
 import ToggleButton from './components/ToggleButton';
 import BarChart from './components/BarChart';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
 
 function App() {
   const [showLineChart, setShowLineChart] = useState(true);
@@ -30,8 +36,17 @@ function App() {
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      <ToggleButton onClickLineChart={handleToggleLineChart} onClickBarChart={handleToggleBarChart} />
+          <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+         <ToggleButton onClickLineChart={handleToggleLineChart} onClickBarChart={handleToggleBarChart} />
       {showLineChart && <LineChart />}
       {showBarChart && <BarChart />}
     </div>
